e3f6cc5a259db027d538c2576c164f8abe419b41
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-11-02 21:01:14 -0500

    changed some of the initial files to reflect the game that is being developed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,43 +1,42 @@
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import SingleThought from './pages/SingleThought';
-import Profile from './pages/Profile';
-import ErrorPage from './pages/ErrorPage';
+import gamePage from './pages/gamePage';
+import homePage from './pages/homePage';
+import signupPage from './pages/signupPage';
+import userPage from './pages/userPage';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      }, {
-        path: '/login',
-        element: <Login />
-      }, {
-        path: '/signup',
-        element: <Signup />
-      }, {
-        path: '/profiles/:username',
-        element: <Profile />
-      }, {
-        path: '/me',
-        element: <Profile />
-      }, {
-        path: '/thoughts/:thoughtId',
-        element: <SingleThought />
-      }
-    ]
-  },
+   {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+         {
+            index: true,
+            element: <homePage />,
+         },
+         {
+            path: '/dice-game',
+            element: <gamePage />,
+         },
+         {
+            path: '/game-page',
+            element: <gamePage />,
+         },
+         {
+            path: '/signup-page',
+            element: <signupPage />,
+         },
+         {
+            path: '/user/:username',
+            element: <userPage />,
+         },
+      ],
+   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+   <RouterProvider router={router} />
+);
