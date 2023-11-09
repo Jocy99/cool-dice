0e6e64272d5fdb695df3ee1f836ef90d656940fd
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-11-09 14:37:36 -0600

    commented out the import statements in the header for testing purposes

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,5 +1,5 @@
-import { useQuery } from '@apollo/client';
-import { Link } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
+// import { Link } from 'react-router-dom';
 
 const navStyles = {
    textAlign: 'center',
