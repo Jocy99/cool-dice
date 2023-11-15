0f5eb1dde1c3a1623617fd7a4f4a63ce9d7711dd
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-11-14 20:42:52 -0600

    added the me function that grabs user paylod from jwt

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,6 +9,15 @@ const resolvers = {
       users: async () => {
          return User.find().populate('scores');
       },
+      user: async (parent, { userId }) => {
+         return User.findOne({ _id: userId });
+      },
+      me: async (parent, args, context) => {
+         if (context.user) {
+            return User.findOne({ _id: context.user._id });
+         }
+         throw AuthenticationError;
+      },
    },
    Mutation: {
       // adds the write functions here
